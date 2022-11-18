98dd39e8f9b427908365d6e7b99bf37b52f3c421
Filippo <pippo.coglio@gmail.com>
2022-11-18 07:06:09 +0100

    Added profile_id

--- authentication/profile/models.py
+++ authentication/profile/models.py
@@ -1,9 +1,12 @@
+import uuid
+
 from django.db import models
 
 from authentication.user.models import User
 
 
 class UserProfile(models.Model):
+    profile_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=20)
