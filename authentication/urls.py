f247a8fa05348500d98004a837a294c760ffca30
Filippo <pippo.coglio@gmail.com>
2022-11-18 07:37:38 +0100

    Added url matching for signup

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
+from authentication.user.views import UserRegistrationView
+
 urlpatterns = [
-    path('token/refresh', TokenRefreshView.as_view())
+    path('token/refresh', TokenRefreshView.as_view()),
+    path('signup', UserRegistrationView.as_view())
 ]
