f6e62581816a62886f20fe2ac10b35e80ecb0f37
Filippo <pippo.coglio@gmail.com>
2022-11-19 07:13:32 +0100

    Added userprofile admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from authentication.profile import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
