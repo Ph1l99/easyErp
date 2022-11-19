51c74e9dc4449196ead3643d2c19e16ad2aa48c8
Filippo <pippo.coglio@gmail.com>
2022-11-19 06:40:00 +0100

    Changed authentication class

--- easyErp/settings.py
+++ easyErp/settings.py
@@ -86,6 +86,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
