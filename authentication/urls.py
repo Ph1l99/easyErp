f9d9cee290b6bfdf242c4af458e88dda05058868
Filippo <pippo.coglio@gmail.com>
2022-11-19 07:55:42 +0100

    Refactored urls for better readability

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,9 @@ from authentication.user.views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
     path('token/refresh', TokenRefreshView.as_view(), name='token_obtain_refresh'),
+
     path('login', UserLoginView.as_view(), name='login'),
     path('signup', UserRegistrationView.as_view(), name='signup'),
+
     path('profile', UserProfileView.as_view(), name='profile')
 ]
