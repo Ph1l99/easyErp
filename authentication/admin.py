9a2db51f2e9e1f4e3495e4625d6bd4755de05680
Filippo <pippo.coglio@gmail.com>
2022-11-19 07:29:38 +0100

    Added fields to user admin panel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 from authentication.profile import UserProfile
+from authentication.user import User
 
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'username')
 
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'is_active', 'is_superuser', 'last_login')
+
+
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(User, UserAdmin)
