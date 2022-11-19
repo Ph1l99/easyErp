12926469daab34e65b693b7d2beeb6ce6163377d
Filippo <pippo.coglio@gmail.com>
2022-11-19 07:00:41 +0100

    Added url path

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from authentication.profile.views import UserProfileView
-from authentication.user.views import UserRegistrationView
+from authentication.user.views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_obtain_refresh'),
+    path('login', UserLoginView.as_view(), name='login'),
     path('signup', UserRegistrationView.as_view(), name='signup'),
     path('profile', UserProfileView.as_view(), name='profile')
 ]
