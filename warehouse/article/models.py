3a2ccfc14391a5338f2bd8a53f13e5c1b18f8c11
Filippo <pippo.coglio@gmail.com>
2022-11-20 17:00:41 +0100

    Reduced barcode length on model

--- warehouse/article/models.py
+++ warehouse/article/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Article(models.Model):
-    barcode = models.CharField(primary_key=True, max_length=128, unique=True)
+    barcode = models.CharField(primary_key=True, max_length=60, unique=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     reorder_threshold = models.IntegerField(default=0)
