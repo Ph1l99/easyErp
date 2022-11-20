40fa569bfe9ecc84e0d3ba30cf591fe6bd4c5d4a
Filippo <pippo.coglio@gmail.com>
2022-11-20 16:59:45 +0100

    Added create method to serializer

--- warehouse/article/serializers.py
+++ warehouse/article/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
+import config
 from warehouse.article import Article
+from warehouse.article.services.article_manager import ArticleManager
 
 
 class ArticleSerializer(serializers.ModelSerializer):
@@ -15,3 +17,20 @@ class ArticleSerializer(serializers.ModelSerializer):
         instance.is_active = validated_data.get('is_active', instance.is_active)
         instance.save()
         return instance
+
+    def create(self, validated_data):
+        if validated_data.get('barcode') == '-1':
+
+            generated_barcode = ArticleManager.generate_unique_barcode(length=config.ARTICLE_BARCODE_LENGTH)
+
+            while Article.objects.filter(barcode=generated_barcode).exists():
+                generated_barcode = ArticleManager.generate_unique_barcode(length=10)
+
+            article = Article.objects.create(barcode=generated_barcode,
+                                             name=validated_data.get('name'),
+                                             description=validated_data.get('description'),
+                                             reorder_threshold=validated_data.get('reorder_threshold'),
+                                             is_active=validated_data.get('is_active'))
+        else:
+            article = Article.objects.create(**validated_data)
+        return article
