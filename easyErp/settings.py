fca198c304f34d4d1f186a6fb4edef5c48591ba2
Filippo <pippo.coglio@gmail.com>
2022-11-20 07:47:28 +0100

    Implemented drf-yasg dependency

--- easyErp/settings.py
+++ easyErp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- easyErp/urls.py
+++ easyErp/urls.py
@@ -14,9 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls'))
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="EasyERP API",
+        default_version='v1',
+        description="EasyERP API",
+        license=openapi.License(name="GNU General Public License v.3.0"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
