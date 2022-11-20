d85ebc03ff0b480a72a513c1ae99dbc4f0d86a6a
Filippo <pippo.coglio@gmail.com>
2022-11-20 17:17:22 +0100

    Added transactionRef to admin

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 from warehouse.article import Article
+from warehouse.transaction.models import TransactionReference
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('name', 'barcode', 'reorder_threshold')
 
 
+class TransactionReferenceAdmin(admin.ModelAdmin):
+    list_display = ('description', 'operation_type', 'is_active')
+
+
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(TransactionReference, TransactionReferenceAdmin)
