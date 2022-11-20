4dbcdbb2c021a93530cee6e0f3c984942d557127
Filippo <pippo.coglio@gmail.com>
2022-11-20 16:25:30 +0100

    Added article serializer update method

--- warehouse/article/serializers.py
+++ warehouse/article/serializers.py
@@ -7,3 +7,11 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.reorder_threshold = validated_data.get('reorder_threshold', instance.reorder_threshold)
+        instance.is_active = validated_data.get('is_active', instance.is_active)
+        instance.save()
+        return instance
