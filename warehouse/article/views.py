3a4f176dbc12329547bf7fbfc838f373eaa7e3de
Filippo <pippo.coglio@gmail.com>
2022-11-20 16:59:59 +0100

    Fix return on post article

--- warehouse/article/views.py
+++ warehouse/article/views.py
@@ -26,6 +26,7 @@ class ArticleView(APIView):
                 serializer = self.serializer_class(data=request.data)
                 serializer.is_valid()
                 serializer.save()
+                return Response(status=status.HTTP_201_CREATED)
             except ValidationError:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_400_BAD_REQUEST)
