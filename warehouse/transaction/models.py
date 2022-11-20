811b00923b13205864e49cad016d4aaac6e0a70f
Filippo <pippo.coglio@gmail.com>
2022-11-20 17:29:26 +0100

    Added transaction detail and transaction models

--- warehouse/transaction/models.py
+++ warehouse/transaction/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 
@@ -10,9 +12,19 @@ class TransactionReference(models.Model):
         return self.description
 
 
-# class TransactionDetail(models.Model):
-#     pass
-#
-#
-# class Transaction(models.Model):
-#     pass
+class TransactionDetail(models.Model):
+    article_identifier = models.CharField(max_length=60)
+    quantity = models.IntegerField(default=1)
+    reference = models.ForeignKey(TransactionReference, on_delete=models.RESTRICT, blank=True)
+
+    def __str__(self):
+        return self.id
+
+
+class Transaction(models.Model):
+    details = models.ManyToManyField(TransactionDetail, related_name='transaction')
+    date_and_time = models.DateTimeField(default=datetime.datetime.utcnow())
+    username = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.date_and_time.strftime('%d-%m-%-yT%H:%M:%S')
