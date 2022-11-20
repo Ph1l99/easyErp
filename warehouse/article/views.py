a3e32169d63a4f33dfb0536297ff95dbcb309fd8
Filippo <pippo.coglio@gmail.com>
2022-11-20 16:25:39 +0100

    Added article get and put view

--- warehouse/article/views.py
+++ warehouse/article/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -11,6 +12,25 @@ from warehouse.article.serializers import ArticleSerializer
 class ArticleView(APIView):
     serializer_class = ArticleSerializer
     permission_classes = [IsAuthenticated]
+
+    def get(self, request, barcode):
+        try:
+            article = Article.objects.get(barcode=barcode)
+            return Response(data=self.serializer_class(article).data, status=status.HTTP_200_OK)
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, barcode):
+        try:
+            article_to_be_updated = Article.objects.get(barcode=barcode)
+            serializer = self.serializer_class(article_to_be_updated, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(status=status.HTTP_200_OK)
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except ValidationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class ArticleListView(ListAPIView):
