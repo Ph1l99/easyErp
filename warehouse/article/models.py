12007e089f6aade2a5c9d224e9631474e935316f
Filippo <pippo.coglio@gmail.com>
2022-11-20 15:05:00 +0100

    Added field to article

--- warehouse/article/models.py
+++ warehouse/article/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     reorder_threshold = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
