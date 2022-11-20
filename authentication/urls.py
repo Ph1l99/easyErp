4bb940c286877f987ed9b61f352329cfbea4c600
Filippo <pippo.coglio@gmail.com>
2022-11-20 08:40:25 +0100

    Added url for change password view

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from authentication.profile.views import UserProfileView
-from authentication.user.views import UserRegistrationView, UserLoginView
+from authentication.user.views import UserRegistrationView, UserLoginView, UserUpdatePasswordView
 
 urlpatterns = [
     path('token/refresh', TokenRefreshView.as_view(), name='token_obtain_refresh'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('login', UserLoginView.as_view(), name='login'),
     path('signup', UserRegistrationView.as_view(), name='signup'),
 
-    path('profile', UserProfileView.as_view(), name='profile')
+    path('profile', UserProfileView.as_view(), name='profile'),
+    path('profile/password', UserUpdatePasswordView.as_view(), name='profile_password')
 ]
