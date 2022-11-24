e35c174b99408b4343276b38c42cbfe3ce9b61db
Filippo <pippo.coglio@gmail.com>
2022-11-24 16:30:27 +0100

    Added routing for transactions

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
 from warehouse.article.views import ListArticleView, ArticleView
+from warehouse.transaction.views import ListTransactionView, CreateTransactionView
 
 urlpatterns = [
     path('articles/', ListArticleView.as_view(), name='articles'),
     path('articles/<str:barcode>', ArticleView.as_view(), name='article')
+]
+
+urlpatterns += [
+    path('transactions/', ListTransactionView.as_view(), name='transactions'),
+    path('transactions/-1', CreateTransactionView.as_view(), name='transaction')
 ]
