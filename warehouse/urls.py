60c05789d63eb0c02a854db6470d9cb575a803ec
Filippo <pippo.coglio@gmail.com>
2022-11-24 15:11:10 +0100

    Fix url path for articles

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from warehouse.article.views import ListArticleView, ArticleView
 
 urlpatterns = [
-    path('articles', ListArticleView.as_view(), name='articles'),
+    path('articles/', ListArticleView.as_view(), name='articles'),
     path('articles/<str:barcode>', ArticleView.as_view(), name='article')
 ]
