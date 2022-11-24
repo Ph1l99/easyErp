a3778f1708a96156f2606e89647f9b9d00afebb4
Filippo <pippo.coglio@gmail.com>
2022-11-24 16:30:05 +0100

    Added transaction admin

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from warehouse.article import Article
-from warehouse.transaction.models import TransactionReference
+from warehouse.transaction.models import TransactionReference, Transaction
 
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -12,5 +12,10 @@ class TransactionReferenceAdmin(admin.ModelAdmin):
     list_display = ('description', 'operation_type', 'is_active')
 
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'date_and_time')
+
+
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(TransactionReference, TransactionReferenceAdmin)
+admin.site.register(Transaction, TransactionAdmin)
