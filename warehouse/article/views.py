7eb3bd853a5c19a7868c610626bf16683f7c6ceb
Filippo <pippo.coglio@gmail.com>
2022-11-24 15:48:09 +0100

    Removed unused imports

--- warehouse/article/views.py
+++ warehouse/article/views.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
-from rest_framework.mixins import ListModelMixin
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
