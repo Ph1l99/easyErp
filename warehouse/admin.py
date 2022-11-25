8ce840601984e9c7dc9fc0b4a1b0c17d5df421c4
Filippo <pippo.coglio@gmail.com>
2022-11-25 18:37:28 +0100

    Added transaction detail admin panel

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from warehouse.article import Article
-from warehouse.transaction.models import TransactionReference, Transaction
+from warehouse.transaction.models import TransactionReference, Transaction, TransactionDetail
 
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -12,10 +12,15 @@ class TransactionReferenceAdmin(admin.ModelAdmin):
     list_display = ('description', 'operation_type', 'is_active')
 
 
+class TransactionDetailAdmin(admin.ModelAdmin):
+    list_display = ('id', 'article', 'quantity', 'reference')
+
+
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'date_and_time')
 
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(TransactionReference, TransactionReferenceAdmin)
+admin.site.register(TransactionDetail, TransactionDetailAdmin)
 admin.site.register(Transaction, TransactionAdmin)
