a68a8619779b1cea31dccfd6369d4e13637184e8
Filippo <pippo.coglio@gmail.com>
2022-11-25 19:02:54 +0100

    Linked view to url mapping

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from warehouse.article.views import ListArticleView, ArticleView
-from warehouse.transaction.views import ListTransactionView, CreateTransactionView
+from warehouse.transaction.views import ListTransactionView, CreateTransactionView, ListTransactionReferenceView
 
 urlpatterns = [
     path('articles/', ListArticleView.as_view(), name='articles'),
@@ -10,5 +10,6 @@ urlpatterns = [
 
 urlpatterns += [
     path('transactions/', ListTransactionView.as_view(), name='transactions'),
-    path('transactions/-1', CreateTransactionView.as_view(), name='transaction')
+    path('transactions/-1', CreateTransactionView.as_view(), name='transaction'),
+    path('transactions/references/', ListTransactionReferenceView.as_view(), name='transactions_references')
 ]
