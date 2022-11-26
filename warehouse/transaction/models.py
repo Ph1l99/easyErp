ab7b2d0bac905c71ac2fb8ac10c821590c62eca2
Filippo <pippo.coglio@gmail.com>
2022-11-26 06:57:13 +0100

    Refactored transaction and details relationships

--- warehouse/transaction/models.py
+++ warehouse/transaction/models.py
@@ -20,17 +20,7 @@ class TransactionReference(models.Model):
         ordering = ['id']
 
 
-class TransactionDetail(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.RESTRICT, default=None)
-    quantity = models.IntegerField(default=1)
-    reference = models.ForeignKey(TransactionReference, on_delete=models.RESTRICT, blank=True)
-
-    def __str__(self):
-        return 'Trans. detail: ' + str(self.id)
-
-
 class Transaction(models.Model):
-    details = models.ManyToManyField(TransactionDetail, related_name='transaction')
     date_and_time = models.DateTimeField(default=timezone.now())
     username = models.CharField(max_length=20)
 
@@ -39,3 +29,13 @@ class Transaction(models.Model):
 
     class Meta:
         ordering = ['-id']
+
+
+class TransactionDetail(models.Model):
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.RESTRICT)
+    quantity = models.IntegerField(default=1)
+    reference = models.ForeignKey(TransactionReference, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return 'Trans. detail: ' + str(self.id)
