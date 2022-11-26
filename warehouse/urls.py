634d1ce88f77343cb79ba043d0d7567851f20aab
Filippo <pippo.coglio@gmail.com>
2022-11-26 07:21:48 +0100

    Fix typo error on urls

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path('transactions/', ListTransactionView.as_view(), name='transactions'),
+    path('transactions', ListTransactionView.as_view(), name='transactions'),
     path('transactions/-1', CreateTransactionView.as_view(), name='transaction'),
     path('transactions/<int:transaction_id>/details', ListTransactionDetailsView.as_view(), name='transaction_details'),
     path('transactions/references', ListTransactionReferenceView.as_view(), name='transactions_references')
