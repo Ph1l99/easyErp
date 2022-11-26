67b045543c51e1bc5065421b944b187ffb30a543
Filippo <pippo.coglio@gmail.com>
2022-11-26 14:36:53 +0100

    Installed cors module, configured for debug

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ if ENV == 'DEV':
     }
     ALLOWED_HOSTS = ['*']
     DEBUG = True
+    CORS_ORIGIN_ALLOW_ALL = DEBUG
     SECRET_KEY = 'django-insecure-286pkv6f3q60t6#58dv!dejud9!k#pa&yi7+pjz-(5n724s84l'
     SECRET_JWT_KEY = 'django-insecure-286pkv6f3q60t6#58dv!dejud9!k#pa&yi7+pjz-(5n724s84l'
     ARTICLE_BARCODE_LENGTH = 24
@@ -24,6 +25,7 @@ elif ENV == 'PRD':
     }
     ALLOWED_HOSTS = []
     DEBUG = False
+    CORS_ORIGIN_ALLOW_ALL = DEBUG
     SECRET_KEY = ''
     SECRET_JWT_KEY = ''
     ARTICLE_BARCODE_LENGTH = 24
--- easyErp/settings.py
+++ easyErp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg'
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
