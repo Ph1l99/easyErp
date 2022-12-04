9e1da69232abeea788844978e1aa371e5d9aba21
Filippo <pippo.coglio@gmail.com>
2022-12-04 10:57:19 +0100

    Added view for listing repairs

--- repair/views.py
+++ repair/views.py
@@ -1,9 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
-from repair.models import RepairStatus
-from repair.serializers import RepairStatusSerializer
+from repair.models import RepairStatus, Repair
+from repair.serializers import RepairStatusSerializer, ListRepairSerializer
 
 
 class ListRepairStatusView(ListAPIView):
@@ -13,3 +14,12 @@ class ListRepairStatusView(ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['is_active']
     queryset = RepairStatus.objects.all()
+
+
+class ListRepairView(ListAPIView):
+    serializer_class = ListRepairSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status']
+    queryset = Repair.objects.all()
