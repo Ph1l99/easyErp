dd6cc7a8660d472562715307dade20ef6dc23ed7
Filippo <pippo.coglio@gmail.com>
2022-12-04 08:52:06 +0100

    Fix get username

--- warehouse/inventory/views.py
+++ warehouse/inventory/views.py
@@ -14,7 +14,7 @@ class CreateInventoryCycleView(CreateAPIView):
     def post(self, request, *args, **kwargs):
         inventory_manager = InventoryManager()
         try:
-            inventory_manager.create_inventory_cycle(username=UserProfile.objects.get(user__id=request.user.id))
+            inventory_manager.create_inventory_cycle(username=UserProfile.objects.get(user__id=request.user.id).username)
         except InventoryManagerNotCreatedException:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_200_OK)
