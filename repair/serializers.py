221d4c358ab258a7b7b3687cbd6ed79923d6a50f
Filippo <pippo.coglio@gmail.com>
2022-12-04 10:57:09 +0100

    Added serializer for listing repairs

--- repair/serializers.py
+++ repair/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 
-from repair.models import RepairStatus
+from repair.models import RepairStatus, Repair
 
 
 class RepairStatusSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = RepairStatus
         fields = '__all__'
+
+
+class ListRepairSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Repair
+        fields = '__all__'
+        depth = 1
