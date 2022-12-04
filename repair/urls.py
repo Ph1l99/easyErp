0231fda1a4883cc54e3dbb98dfe32e67383ec921
Filippo <pippo.coglio@gmail.com>
2022-12-04 10:57:29 +0100

    Added url for repair view list

--- repair/urls.py
+++ repair/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from repair.views import ListRepairStatusView
+from repair.views import ListRepairStatusView, ListRepairView
 
 urlpatterns = [
+    path('', ListRepairView.as_view(), name='repairs'),
     path('status', ListRepairStatusView.as_view(), name='repair_status')
 ]
