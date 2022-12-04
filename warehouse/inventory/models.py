40607f3c4a1a62e9d57e7d8f11249f9e433ec5fe
Filippo <pippo.coglio@gmail.com>
2022-12-04 08:52:24 +0100

    Added str method to models

--- warehouse/inventory/models.py
+++ warehouse/inventory/models.py
@@ -8,6 +8,9 @@ class InventoryCycle(models.Model):
     date = models.DateField(default=timezone.now)
     username = models.CharField(max_length=20)
 
+    def __str__(self):
+        return 'Inv. cycle: ' + str(self.id) + ' date: ' + self.date.strftime('%d-%m-%-y')
+
     class Meta:
         ordering = ['-date']
 
@@ -16,3 +19,6 @@ class InventoryCycleDetail(models.Model):
     article = models.ForeignKey(Article, on_delete=models.RESTRICT)
     quantity = models.IntegerField(default=0)
     cycle = models.ForeignKey(InventoryCycle, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Id: ' + str(self.id) + ' article: ' + str(self.article.barcode) + ' qty: ' + str(self.quantity)
