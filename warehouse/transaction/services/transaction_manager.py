b5113648e3375600c36ce2c552f7977cad9660d1
Filippo <pippo.coglio@gmail.com>
2022-12-04 09:32:17 +0100

    Added check quantity in transaction manager

--- warehouse/transaction/services/transaction_manager.py
+++ warehouse/transaction/services/transaction_manager.py
@@ -1,36 +1,40 @@
-from rest_framework.exceptions import ValidationError
-
+from warehouse.inventory.services.inventory_manager import InventoryManager
+from warehouse.transaction.exceptions import TransactionQuantityNotConsistentException
 from warehouse.transaction.models import TransactionDetail, Transaction
 
 
 class TransactionManager:
-    def create_transaction_and_details_from_validated_data(self, validated_data, details):
+    def create_transaction_and_details(self, validated_data, details):
 
         transaction = Transaction.objects.create(username=validated_data['username'])
 
         if transaction is not None:
-            created_details = []
             for transaction_detail in details:
-                try:
-                    created_transaction_detail = TransactionDetail.objects.create(article=transaction_detail['article'],
-                                                                                  quantity=transaction_detail[
-                                                                                      'quantity'],
-                                                                                  reference=transaction_detail[
-                                                                                      'reference'],
-                                                                                  transaction=transaction)
-                    # todo check availability in order to provide consistency
-                    created_details.append(created_transaction_detail)
-                except Exception:
-                    if len(created_details) > 0:
-                        self._rollback_transaction_detail_creation(created_details=created_details)
-                        created_details = []
-                        transaction.delete()
-                        return None
+                # Extract the current article quantity and the one being transacted
+                current_quantity_for_article = InventoryManager().get_current_quantity_for_article(
+                    transaction_detail['article'].barcode)
+                quantity_being_transacted = int(
+                    transaction_detail['reference'].operation_type + str(transaction_detail['quantity']))
 
+                if self._is_quantity_consistent(current_quantity=current_quantity_for_article,
+                                                quantity_being_transacted=quantity_being_transacted):
+                    TransactionDetail.objects.create(article=transaction_detail['article'],
+                                                     quantity=transaction_detail[
+                                                         'quantity'],
+                                                     reference=transaction_detail[
+                                                         'reference'],
+                                                     transaction=transaction)
+                else:
+                    # If the quantity is not consistent, the whole transaction is rolled back
+                    transaction.delete()
+                    raise TransactionQuantityNotConsistentException
             return transaction
 
-        return None
+        raise Transaction.DoesNotExist
 
-    def _rollback_transaction_detail_creation(self, created_details):
-        for created_detail in created_details:
-            TransactionDetail.objects.filter(id=created_detail.id).delete()
+    def _is_quantity_consistent(self, current_quantity, quantity_being_transacted):
+        return quantity_being_transacted != 0 and \
+            (current_quantity <= 0 and
+             current_quantity + quantity_being_transacted > current_quantity) or \
+            (current_quantity >= 0 and
+             current_quantity + quantity_being_transacted >= 0)
