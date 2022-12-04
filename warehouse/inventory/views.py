8a3df78545a8a7d4c14a94849c311a0c21c2936a
Filippo <pippo.coglio@gmail.com>
2022-12-04 08:13:59 +0100

    Added view for inventory

--- warehouse/inventory/views.py
+++ warehouse/inventory/views.py
@@ -3,9 +3,18 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from authentication.profile import UserProfile
+from warehouse.inventory.exceptions import InventoryManagerNotCreatedException
+from warehouse.inventory.services.inventory_manager import InventoryManager
+
 
 class CreateInventoryCycleView(CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        inventory_manager = InventoryManager()
+        try:
+            inventory_manager.create_inventory_cycle(username=UserProfile.objects.get(user__id=request.user.id))
+        except InventoryManagerNotCreatedException:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)
