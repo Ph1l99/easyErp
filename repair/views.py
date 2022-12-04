8cd56ff54fb4a2dcd5b1f407ec98d76acef70353
Filippo <pippo.coglio@gmail.com>
2022-12-04 10:39:45 +0100

    Created view for repair status

--- repair/views.py
+++ repair/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from repair.models import RepairStatus
+from repair.serializers import RepairStatusSerializer
+
+
+class ListRepairStatusView(ListAPIView):
+    serializer_class = RepairStatusSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['is_active']
+    queryset = RepairStatus.objects.all()
