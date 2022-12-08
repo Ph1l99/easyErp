bc7d1365040feb639cbbd16d0265b07da5ce8f71
Filippo <pippo.coglio@gmail.com>
2022-12-08 08:27:06 +0100

    Mapped url for repair

--- repair/urls.py
+++ repair/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from repair.views import ListRepairStatusView, ListRepairView
+from repair.views import ListRepairStatusView, ListRepairView, CreateEditGetRepairView
 
 urlpatterns = [
     path('', ListRepairView.as_view(), name='repairs'),
-    path('status', ListRepairStatusView.as_view(), name='repair_status')
+    path('status', ListRepairStatusView.as_view(), name='repair_status'),
+    path('<str:barcode>', CreateEditGetRepairView.as_view(), name='repair')
 ]
