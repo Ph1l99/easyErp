65de411eb4c650df8439ba89f128d28f8c07f087
Filippo <pippo.coglio@gmail.com>
2022-12-15 16:30:25 +0100

    Added admin for customer app

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from customer.models import FidelityCard, Customer
+
+
+class FidelityCardAdmin(admin.ModelAdmin):
+    list_display = ('barcode', 'is_active')
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+
+
+admin.site.register(FidelityCard, FidelityCardAdmin)
+admin.site.register(Customer, CustomerAdmin)
