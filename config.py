38200cdfd066b3dbd96aad8d8906b3dfeadd3003
Filippo <pippo.coglio@gmail.com>
2022-12-15 15:45:49 +0100

    Added printing parameters in config

--- config.py
+++ config.py
@@ -3,19 +3,29 @@ import os
 ENV = os.environ.get('ENV', 'DEV')
 
 if ENV == 'DEV':
+    # DB
     DATABASE = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'db.sqlite3',
     }
+    # Generic settings
     ALLOWED_HOSTS = ['*']
     DEBUG = True
     CORS_ORIGIN_ALLOW_ALL = DEBUG
     SECRET_KEY = 'django-insecure-286pkv6f3q60t6#58dv!dejud9!k#pa&yi7+pjz-(5n724s84l'
     SECRET_JWT_KEY = 'django-insecure-286pkv6f3q60t6#58dv!dejud9!k#pa&yi7+pjz-(5n724s84l'
+    # Barcode settings
     ARTICLE_BARCODE_LENGTH = 24
     REPAIR_BARCODE_LENGTH = 15
+    # Thermal printer settings
+    THERMAL_PRINTER_VENDOR_ID = 0x04b8
+    THERMAL_PRINTER_PRODUCT_ID = 0x0202
+    THERMAL_PRINTER_HEADER = 'FEDE\'S ELECTRONICS\n'
+    THERMAL_PRINTER_SUB_HEADER = 'di Ossola Federico\n\n'
+    THERMAL_PRINTER_FOOTER_REPAIR = 'Porta con te questo scontrino per ritirare la tua riparazione!\n'
 
 elif ENV == 'PRD':
+    # DB
     DATABASE = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'easyErp',
@@ -24,10 +34,19 @@ elif ENV == 'PRD':
         'HOST': os.environ.get('DB_HOST'),
         'PORT': 5432
     }
+    # Generic settings
     ALLOWED_HOSTS = []
     DEBUG = False
     CORS_ORIGIN_ALLOW_ALL = DEBUG
     SECRET_KEY = ''
     SECRET_JWT_KEY = ''
+    # Barcode settings
     ARTICLE_BARCODE_LENGTH = 24
     REPAIR_BARCODE_LENGTH = 15
+    # Thermal printer settings
+    THERMAL_PRINTER_VENDOR_ID = 0x04b8
+    THERMAL_PRINTER_PRODUCT_ID = 0x0202
+    # Printing layout
+    THERMAL_PRINTER_HEADER = 'FEDE\'S ELECTRONICS\n'
+    THERMAL_PRINTER_SUB_HEADER = 'di Ossola Federico\n\n'
+    THERMAL_PRINTER_FOOTER_REPAIR = 'Porta con te questo scontrino per ritirare la tua riparazione!\n'
