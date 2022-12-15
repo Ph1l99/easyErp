5b035ff9bf7dfe65bef0352f5471dc010df70e18
Filippo <pippo.coglio@gmail.com>
2022-12-15 16:15:16 +0100

    Added customer and fidelity card

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class FidelityCard(models.Model):
+    barcode = models.CharField(max_length=50, primary_key=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.barcode
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=30)
+    fidelity_card = models.ForeignKey(FidelityCard, on_delete=models.RESTRICT, blank=True)
+
+    def __str__(self):
+        return ' '.join([self.last_name, self.first_name])
