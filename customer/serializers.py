e45af1ec701475d8b39efad6a6041e3558aa38eb
Filippo <pippo.coglio@gmail.com>
2022-12-17 07:52:53 +0100

    Added field list customer serializer

--- customer/serializers.py
+++ customer/serializers.py
@@ -6,7 +6,7 @@ from customer.models import Customer, FidelityCard
 class ListCustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['first_name', 'last_name']
+        fields = ['id', 'first_name', 'last_name']
 
 
 class ListFidelityCardSerializer(serializers.ModelSerializer):
