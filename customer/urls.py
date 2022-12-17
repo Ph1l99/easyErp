92a7dc21943f0c15e0e16b0c76c3e7337801a9f3
Filippo <pippo.coglio@gmail.com>
2022-12-17 07:54:36 +0100

    Refactored urls for customer

--- customer/urls.py
+++ customer/urls.py
@@ -4,6 +4,6 @@ from customer.views import ListCustomerView, CustomerView, ListFidelityCardView
 
 urlpatterns = [
     path('', ListCustomerView.as_view(), name='customers'),
-    path('<str:customer_id>', CustomerView.as_view(), name='customer'),
+    path('customer/<str:customer_id>', CustomerView.as_view(), name='customer'),
     path('fidelityCards', ListFidelityCardView.as_view(), name='fidelity_cards')
 ]
