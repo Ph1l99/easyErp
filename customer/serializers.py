11cbb1f8507e74e9fe71cf1e29f9bffa27881292
Filippo <pippo.coglio@gmail.com>
2022-12-17 07:49:30 +0100

    Added customer serializers

--- customer/serializers.py
+++ customer/serializers.py
@@ -13,3 +13,16 @@ class ListFidelityCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = FidelityCard
         fields = '__all__'
+
+
+class CreateUpdateCustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['first_name', 'last_name', 'phone', 'fidelity_card']
+
+
+class GetCustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        depth = 1
