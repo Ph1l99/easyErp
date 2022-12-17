d9bac96b4e8d3bb1972dd3cc297b2ebeaaefc6d0
Filippo <pippo.coglio@gmail.com>
2022-12-17 07:49:39 +0100

    Added customer serializer view

--- customer/urls.py
+++ customer/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from customer.views import ListCustomerView, CustomerView, ListFidelityCardView
+
+urlpatterns = [
+    path('', ListCustomerView.as_view(), name='customers'),
+    path('<str:customer_id>', CustomerView.as_view(), name='customer'),
+    path('fidelityCards', ListFidelityCardView.as_view(), name='fidelity_cards')
+]
--- customer/views.py
+++ customer/views.py
@@ -1,11 +1,16 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from customer.models import Customer, FidelityCard
-from customer.serializers import ListCustomerSerializer, ListFidelityCardSerializer
+from customer.serializers import ListCustomerSerializer, ListFidelityCardSerializer, CreateUpdateCustomerSerializer, \
+    GetCustomerSerializer
 
 
 class ListCustomerView(ListAPIView):
@@ -24,3 +29,49 @@ class ListFidelityCardView(ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['is_active']
     queryset = FidelityCard.objects.all()
+
+
+class CustomerView(APIView):
+    serializer_class = CreateUpdateCustomerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, customer_id):
+
+        try:
+            Customer.objects.get(id=customer_id)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except Customer.DoesNotExist:
+            try:
+                serializer = self.serializer_class(data=request.data)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(status=status.HTTP_201_CREATED)
+            except ValidationError:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, customer_id):
+        try:
+            customer = Customer.objects.get(id=customer_id)
+            serializer = self.serializer_class(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(status=status.HTTP_200_OK)
+        except Customer.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except ValidationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, customer_id):
+        try:
+            customer = Customer.objects.get(id=customer_id)
+            customer.delete()
+            return Response(status=status.HTTP_200_OK)
+        except Customer.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, customer_id):
+        try:
+            customer = Customer.objects.get(id=customer_id)
+            return Response(data=GetCustomerSerializer(customer).data, status=status.HTTP_200_OK)
+        except Customer.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
