388a11562a5b6355966401c8242248db09376480
Filippo <pippo.coglio@gmail.com>
2022-12-18 06:47:04 +0100

    Added custom pagination class

---
+++ core/easy_erp_page_number_pagination.py
@@ -0,0 +1,21 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.utils.urls import replace_query_param, remove_query_param
+
+
+class EasyErpPageNumberPagination(PageNumberPagination):
+
+    def get_next_link(self):
+        if not self.page.has_next():
+            return None
+        url = self.request.get_full_path()
+        page_number = self.page.next_page_number()
+        return replace_query_param(url, self.page_query_param, page_number)
+
+    def get_previous_link(self):
+        if not self.page.has_previous():
+            return None
+        url = self.request.get_full_path()
+        page_number = self.page.previous_page_number()
+        if page_number == 1:
+            return remove_query_param(url, self.page_query_param)
+        return replace_query_param(url, self.page_query_param, page_number)
