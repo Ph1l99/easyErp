8cd18110621b80c386b579f8b341f82d69d28c68
Filippo <pippo.coglio@gmail.com>
2022-12-24 07:02:14 +0100

    Added fidelity card service class

---
+++ customer/services/fidelity_card_service.py
@@ -0,0 +1,7 @@
+class FidelityCardService:
+
+    def is_fidelity_card_available(self, fidelity_card):
+        return False
+
+    def is_fidelity_card_linked_to_customer(self, fidelity_card):
+        return False
