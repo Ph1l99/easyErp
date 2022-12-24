d2010db47da4b84a3ccf39644a43923a07972928
Filippo <pippo.coglio@gmail.com>
2022-12-24 07:27:03 +0100

    Added fidelity card service methods implementation

--- customer/services/fidelity_card_service.py
+++ customer/services/fidelity_card_service.py
@@ -1,7 +1,19 @@
+from customer.exceptions import FidelityCardAlreadyAssignedException
+from customer.models import FidelityCard
+
+
 class FidelityCardService:
 
-    def is_fidelity_card_available(self, fidelity_card):
-        return False
+    def is_fidelity_card_available(self, barcode):
+        try:
+            FidelityCard.objects.get(is_active=True, barcode=barcode, customer__isnull=True)
+        except FidelityCard.DoesNotExist:
+            raise FidelityCardAlreadyAssignedException
+        return True
 
-    def is_fidelity_card_linked_to_customer(self, fidelity_card):
+    def is_fidelity_card_linked_to_customer(self, barcode):
+        try:
+            FidelityCard.objects.get(barcode=barcode, customer__isnull=True)
+        except FidelityCard.DoesNotExist:
+            raise FidelityCardAlreadyAssignedException
         return False
