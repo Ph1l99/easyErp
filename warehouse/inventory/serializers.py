f908c09e134f98c9f73e787db802307711e5d0df
Filippo <pippo.coglio@gmail.com>
2022-12-24 08:55:49 +0100

    Fix serializer type

--- warehouse/inventory/serializers.py
+++ warehouse/inventory/serializers.py
@@ -2,5 +2,5 @@ from rest_framework import serializers
 
 
 class NextInventoryCycleSerializer(serializers.Serializer):
-    last_inventory_cycle = serializers.DateTimeField()
-    next_inventory_cycle = serializers.DateTimeField()
+    last_inventory_cycle = serializers.DateField()
+    next_inventory_cycle = serializers.DateField()
