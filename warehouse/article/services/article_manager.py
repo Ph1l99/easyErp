bebff020b5468480fbb760718037773dc7b6e87f
Filippo <pippo.coglio@gmail.com>
2022-12-27 14:47:32 +0100

    Removed chars from article barcode

--- warehouse/article/services/article_manager.py
+++ warehouse/article/services/article_manager.py
@@ -5,4 +5,4 @@ import string
 class ArticleManager:
     @classmethod
     def generate_unique_barcode(cls, length: int):
-        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+        return ''.join(random.choices(string.digits, k=length))
