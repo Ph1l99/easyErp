f1ac7bd0b891da3d83abe973234cf893d10ad467
Filippo <pippo.coglio@gmail.com>
2022-12-30 07:13:17 +0100

    Changed page size

--- easyErp/settings.py
+++ easyErp/settings.py
@@ -104,7 +104,7 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
