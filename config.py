b257fca305e752dc3e1fb3daa5f05fedae0a8879
Filippo <pippo.coglio@gmail.com>
2023-01-15 17:10:59 +0100

    Fix config

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ if ENV == 'DEV':
     SECRET_JWT_KEY = 'django-insecure-286pkv6f3q60t6#58dv!dejud9!k#pa&yi7+pjz-(5n724s84l'
     # Barcode settings
     ARTICLE_BARCODE_LENGTH = 24
-    REPAIR_BARCODE_LENGTH = 15
+    REPAIR_BARCODE_LENGTH = 8
     # Thermal printer settings
-    THERMAL_PRINTER_VENDOR_ID = 0x04b8
-    THERMAL_PRINTER_PRODUCT_ID = 0x0202
+    THERMAL_PRINTER_VENDOR_ID = 0x0456
+    THERMAL_PRINTER_PRODUCT_ID = 0x0808
     THERMAL_PRINTER_HEADER = 'FEDE\'S ELECTRONIC\n'
     THERMAL_PRINTER_SUB_HEADER = 'di Ossola Federico\n'
-    THERMAL_PRINTER_SUB_HEADER_CONTACTS = 'Contrada Tagliabo\', 16 - 21034 Cocquio-Trevisago (VA)\nTel. 03321696500\n\n'
+    THERMAL_PRINTER_SUB_HEADER_CONTACTS = 'Contrada Tagliabo\', 16\n21034 Cocquio-Trevisago (VA)\nTel. 03321696500\n\n'
     THERMAL_PRINTER_FOOTER_REPAIR = 'Porta con te questo scontrino per ritirare la tua riparazione!\n'
     # Label printer settings
     LABEL_PRINTER_MODEL = 'QL-700'
@@ -53,14 +53,14 @@ elif ENV == 'PRD':
     SECRET_JWT_KEY = ''
     # Barcode settings
     ARTICLE_BARCODE_LENGTH = 24
-    REPAIR_BARCODE_LENGTH = 15
+    REPAIR_BARCODE_LENGTH = 8
     # Thermal printer settings
-    THERMAL_PRINTER_VENDOR_ID = 0x04b8
-    THERMAL_PRINTER_PRODUCT_ID = 0x0202
+    THERMAL_PRINTER_VENDOR_ID = 0x0456
+    THERMAL_PRINTER_PRODUCT_ID = 0x0808
     # Printing layout
     THERMAL_PRINTER_HEADER = 'FEDE\'S ELECTRONIC\n'
     THERMAL_PRINTER_SUB_HEADER = 'di Ossola Federico\n'
-    THERMAL_PRINTER_SUB_HEADER_CONTACTS = 'Contrada Tagliabo\', 16 - 21034 Cocquio-Trevisago (VA)\nTel. 03321696500\n\n'
+    THERMAL_PRINTER_SUB_HEADER_CONTACTS = 'Contrada Tagliabo\', 16\n21034 Cocquio-Trevisago (VA)\nTel. 03321696500\n\n'
     THERMAL_PRINTER_FOOTER_REPAIR = 'Porta con te questo scontrino per ritirare la tua riparazione!\n'
     # Label printer settings
     LABEL_PRINTER_MODEL = 'QL-700'
